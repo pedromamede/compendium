f6d06d3d1733efe4bc7516e8a5d0916741926651
Pedro Mamede <pedro.mamede@gmail.com>
2021-08-28 19:29:54 -0300

    fix crawler

--- app/workers/shortener/title_crawler_worker.rb
+++ app/workers/shortener/title_crawler_worker.rb
@@ -4,8 +4,7 @@ class Shortener::TitleCrawlerWorker
   def perform shortener_id
     shortener = Shortener.find(shortener_id)
     html_doc = HTTParty.get(shortener.full_url_redirect, {timeout: 5}).body
-    Nokogiri::HTML::Document.parse(html_doc).title
-    title = Nokogiri::HTML::Document.parse(HTTParty.get()&.body)&.title
-    shortener.update(title: title) if title
+    title = Nokogiri::HTML::Document.parse(html_doc).title
+    shortener.update(html_title: title) if title
   end
 end
