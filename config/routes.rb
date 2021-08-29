001d13c0b42dc2f4b46703c27bd756d1f4ad785e
Pedro Mamede <pedro.mamede@gmail.com>
2021-08-28 21:11:25 -0300

    sidekiq web UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  require 'sidekiq/cron/web'
+
   root 'shorteners#new'
   
   resources :shorteners, only: [:index, :new, :create]
 
   get '/:short_url', to: 'shorteners#show'
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end
+  
+  mount Sidekiq::Web, at: "/sidekiq"
 end
